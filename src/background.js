412e9c2f8bbd24fd31870beeb47b242a475b3194
Francois Beaufort <beaufort.francois@gmail.com>
2018-06-21 16:01:41 +0200

    Use leavepictureinpicture event

--- src/background.js
+++ src/background.js
@@ -8,13 +8,12 @@ if (!document.pictureInPictureEnabled) {
         
         if (video.hasAttribute('__pip__')) {
           await document.exitPictureInPicture();
-          video.removeAttribute('__pip__');
         } else {
           await video.requestPictureInPicture();
           video.setAttribute('__pip__', true);
-          video.addEventListener('emptied', event => {
+          video.addEventListener('leavepictureinpicture', event => {
             video.removeAttribute('__pip__');
-          });
+          }, { once: true });
         }
       })();
     `;

7f1824524898c0e517b9e0841922cc8e59047481
Francois Beaufort <beaufort.francois@gmail.com>
2019-01-10 10:45:24 +0000

    Pick the biggest video displayed to enter PiP

--- src/background.js
+++ src/background.js
@@ -21,7 +21,11 @@ if (!document.pictureInPictureEnabled) {
         const videos = Array.from(document.querySelectorAll('video'))
             .filter(video => video.readyState != 0)
             .filter(video => video.disablePictureInPicture == false)
-            .sort((v1, v2) => (v2.videoWidth * v2.videoHeight) - (v1.videoWidth * v1.videoHeight));
+            .sort((v1, v2) => {
+              const v1Rect = v1.getClientRects()[0];
+              const v2Rect = v1.getClientRects()[0];
+              return ((v2Rect.width * v2Rect.height) -(v1Rect.width * v1Rect.height));
+            });
 
         if (videos.length === 0)
           return;

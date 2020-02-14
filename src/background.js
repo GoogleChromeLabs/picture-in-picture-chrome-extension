fb1e57a7d076c5f7cc4d943ab8a704dca9f1717a
Chris Kelley <chris.kelley@Chris-Kelleys-BabylonHealth-MacBook-Pro-1583.local>
2020-02-14 10:46:49 -0600

    removed all GA code

--- src/background.js
+++ src/background.js
@@ -19,20 +19,3 @@ if (!document.pictureInPictureEnabled) {
     chrome.tabs.executeScript({ file: 'script.js', allFrames: true });
   });
 }
-
-// var _gaq = _gaq || [];
-// _gaq.push(['_setAccount', 'UA-134864766-1']);
-
-// chrome.runtime.onMessage.addListener(data => {
-//   if (data.message === 'enter')
-//     _gaq.push(['_trackPageview']);
-// });
-
-// chrome.storage.sync.get({ optOutAnalytics: false }, results => {
-//   if (results.optOutAnalytics) {
-//     return;
-//   }
-//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//   ga.src = 'https://ssl.google-analytics.com/ga.js';
-//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-// });

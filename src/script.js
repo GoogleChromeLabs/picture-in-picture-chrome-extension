730bd1aaada35033712604dfd59eaf602911ef57
Prawira GENESTONLIA <prawira@gmail.com>
2021-02-27 19:54:37 +0800

    enabling picture in picture

--- src/script.js
+++ src/script.js
@@ -15,15 +15,18 @@
 function findLargestPlayingVideo() {
   const videos = Array.from(document.querySelectorAll('video'))
     .filter(video => video.readyState != 0)
-    .filter(video => video.disablePictureInPicture == false)
     .sort((v1, v2) => {
-      const v1Rect = v1.getClientRects()[0]||{width:0,height:0};
-      const v2Rect = v2.getClientRects()[0]||{width:0,height:0};
+      const v1Rect = v1.getClientRects()[0] || { width: 0, height: 0 };
+      const v2Rect = v2.getClientRects()[0] || { width: 0, height: 0 };
       return ((v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height));
     });
 
   if (videos.length === 0) {
     return;
+  }
+
+  if (videos[0].disablePictureInPicture == true) {
+    videos[0].disablePictureInPicture = false;
   }
 
   return videos[0];
